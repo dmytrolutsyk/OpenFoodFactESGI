feec46b891a9d4d510fc9f41926d5429ebb712f1
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2021-07-01 22:13:55 +0200

    scan code barre ok

--- app/src/main/java/com/example/openfoodfactesgi/MainActivity.kt
+++ app/src/main/java/com/example/openfoodfactesgi/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.openfoodfactesgi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var scanButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanButton = findViewById(R.id.btn_scan)
+        scanButton.setOnClickListener{
+            val scanner = IntentIntegrator(this)
+            scanner.setDesiredBarcodeFormats(IntentIntegrator.CODE_128)
+            scanner.setBeepEnabled(false)
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.originalIntent, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 }
