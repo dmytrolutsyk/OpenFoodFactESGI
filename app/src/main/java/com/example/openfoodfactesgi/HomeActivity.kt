20325657fe980d24be49d0e1be901bb9e32dbd1d
DESKTOP-L0PK0R7\arnau <arnaud.salomon20@gmail.com>
2021-07-28 01:12:18 +0200

    starting to add remote config firebase

--- app/src/main/java/com/example/openfoodfactesgi/HomeActivity.kt
+++ app/src/main/java/com/example/openfoodfactesgi/HomeActivity.kt
@@ -8,10 +8,15 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.openfoodfactesgi.databinding.ActivityHomeBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var remoteConfig: FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,12 @@ class HomeActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
-
+        remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
         //    FragmentManager manager = getSupportFragmentManager();
         //    manager.beginTransaction().replace(R.id.your_fragment_layout_name, new Fragment1()).commit();
 
